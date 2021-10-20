a1fb8f832e151b44a2380e4c3f5a973953df8b8d
AMIrene <f.adammanik@gmail.com>
2021-10-20 21:43:18 +1100

    removed unused saveBook in the api route

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -4,7 +4,7 @@ import { SAVE_BOOK } from '../utils/mutations';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -16,7 +16,7 @@ const SearchBooks = () => {
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
-  const [saveABook, { error }] = useMutation(SAVE_BOOK);
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
 
   
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
@@ -65,16 +65,16 @@ const SearchBooks = () => {
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
     // get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    // const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    if (!token) {
-      return false;
-    }
+    // if (!token) {
+    //   return false;
+    // }
 
     try {
       // const response = await saveBook(bookToSave, token);
 
-      const response = await saveABook({variables: {...bookToSave} })
+      const response = await saveBook({variables: {...bookToSave} })
 
       if (!response.ok) {
         throw new Error('something went wrong!');
