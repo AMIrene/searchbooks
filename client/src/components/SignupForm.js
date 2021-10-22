99e3cb4206ca1e755a270a5a93344f150c8d33f8
AMIrene <f.adammanik@gmail.com>
2021-10-22 14:28:22 +1100

    ADD_USER mutation functionality to sign up form

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,10 +1,23 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
+//useMutation hook set
+const [addUser, { error, data }] = useMutation(ADD_USER);
+  
+
+const [formState, setFormState] = useState({
+  username: '',
+  email: '',
+  password: '',
+});
+  
+
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
   // set state for form validation
@@ -28,18 +41,16 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const { data } = await addUser({
+        variables: { ...formState },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
+     
+
     }
 
     setUserFormData({
