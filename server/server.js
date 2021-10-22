774259f093e68abf652eea66bfb1eb7f25c6155c
AMIrene <f.adammanik@gmail.com>
2021-10-23 09:53:50 +1100

    added auth middleware to server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
+
+const { authMiddleware } = require('./utils/auth');
+
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -11,6 +14,8 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
+
 });
 
 server.applyMiddleware({ app });
